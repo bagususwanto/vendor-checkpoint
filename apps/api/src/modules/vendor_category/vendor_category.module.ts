9ca5a13cb5c8178a5a4a462bf848d0b674c61910
@bagus_uswanto <bagoes.1933@gmail.com>
2026-04-16 13:23:17 +0700

    feat: import AuditModule and PrismaModule into VendorCategoryModule

--- apps/api/src/modules/vendor_category/vendor_category.module.ts
+++ apps/api/src/modules/vendor_category/vendor_category.module.ts
@@ -2,7 +2,11 @@ import { Module } from '@nestjs/common';
 import { VendorCategoryService } from './vendor_category.service';
 import { VendorCategoryController } from './vendor_category.controller';
 
+import { AuditModule } from '../audit/audit.module';
+import { PrismaModule } from 'src/common/prisma/prisma.module';
+
 @Module({
+  imports: [AuditModule, PrismaModule],
   controllers: [VendorCategoryController],
   providers: [VendorCategoryService],
   exports: [VendorCategoryService],
