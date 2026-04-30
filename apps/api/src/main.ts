4a01c40bd2d0e8bb2caeac27e90b1da76a30b41c
@bagus_uswanto <bagoes.1933@gmail.com>
2026-04-30 17:05:03 +0700

    feat: add configurable port environment variable and log server startup address

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -39,7 +39,9 @@ async function bootstrap() {
     .getInstance()
     .set('query parser', (str: string) => qs.parse(str));
 
-  await app.listen(3000);
+  const port = process.env.PORT || 3000;
+  await app.listen(port);
+  console.log(`Application is running on: http://localhost:${port}`);
 }
 
 void bootstrap();
