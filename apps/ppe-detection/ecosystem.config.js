59f696b6a5f76f13464ea2a6c5c1706316e63de1
@bagus_uswanto <bagoes.1933@gmail.com>
2026-04-30 22:56:35 +0700

    chore: set working directory to current path in ecosystem config

--- apps/ppe-detection/ecosystem.config.js
+++ apps/ppe-detection/ecosystem.config.js
@@ -5,6 +5,7 @@ module.exports = {
       script: "venv\\Scripts\\python.exe",
       args: "-m uvicorn main:app --host 0.0.0.0 --port 8000",
       interpreter: "none",
+      cwd: ".",
       env: {
         NODE_ENV: "production",
       }
